bac8f21c2a00c4e9749dd73764c2621afd32f17d
barteaga <barteaga@umich.edu>
2020-05-24 10:35:18 -0400

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Ben Arteaga on 5/24/20.
+//  Copyright © 2020 Ben Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
